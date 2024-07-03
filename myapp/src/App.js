82e4afc0c0359ef13baec3a8f29cc56e848b16ea
saurabh4323 <saurabhiitr01@gmail.com>
2024-07-03 16:31:19 +0530

    created contact us , styled job form , added router

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Register } from "./Register/Register";
 import Job from "./Job/Job";
 import { Login } from "./Login/Login";
+import Myjob from "./Job/Myjob";
+import Contact from "./contact/Contact";
 
 function App() {
   return (
@@ -13,8 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing></Landing>} />
           <Route path="/register" element={<Register></Register>} />
-          <Route path="/login" element={<Login/>} />
+          <Route path="/login" element={<Login />} />
           <Route path="/job" element={<Job></Job>} />
+          <Route path="/contact" element={<Contact></Contact>} />
         </Routes>
       </div>
     </BrowserRouter>
